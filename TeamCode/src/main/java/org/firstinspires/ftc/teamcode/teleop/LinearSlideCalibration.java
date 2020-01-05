d1443f9eaa406ce84b560a96501aa0545a8752e3
Josh <10765599+meow10811@users.noreply.github.com>
2020-01-05 10:47:20 -0500

    made it play sound

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LinearSlideCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LinearSlideCalibration.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.ftccommon.SoundPlayer;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -11,5 +12,21 @@ public class LinearSlideCalibration extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+        // Send diagnostics to user
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        // Initialize the robot hardware
+        Kevin.init(hardwareMap);
+
+        // Send diagnostics to user
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        Kevin.linearSlides.setPosition(0);
+        SoundPlayer.getInstance().startPlaying(hardwareMap.appContext,
+                hardwareMap.appContext.getResources().getIdentifier("ss_alarm", "raw", hardwareMap.appContext.getPackageName()));
     }
 }
